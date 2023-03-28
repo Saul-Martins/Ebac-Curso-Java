738a377dedf9fea4e76f038344f36faf512b8d1a
Luxurie <saulmartinezsilva@outlook.com>
2023-03-27 22:45:49 -0300

    tarefas-ebac-objeto-real

---
+++ tarefas-ebac/src/Objeto.java
@@ -0,0 +1,11 @@
+public class Objeto {
+
+    public static void main(String[] args){
+        Piano piano = new Piano();
+
+        piano.Formato = "Classico";
+        piano.Material = "Madeira Plastificada";
+        piano.NumeroDeCordas = 220;
+        piano.NumeroDeTeclas = 88;
+    }
+}
---
+++ tarefas-ebac/src/Piano.java
@@ -0,0 +1,14 @@
+public class Piano {
+
+        public int NumeroDeTeclas;
+        public String Material;
+        public String Formato;
+        public int NumeroDeCordas;
+
+        public void tocarMusica(){
+        }
+
+
+    }
+
+}
