dfac94b8900cdbaf4a851d1811a10daf6b518966
Luxurie <saulmartinezsilva@outlook.com>
2023-03-29 15:13:18 -0300

    ebac tarefa modulo9

---
+++ Mod9/src/TarefaEbacCasting.java
@@ -0,0 +1,13 @@
+public class TarefaEbacCasting {
+
+
+    public static void main(String args[]) {
+
+        int numero1 = 4;
+        Integer numero2 = numero1;
+
+
+        System.out.println(numero1 + " e " + numero2);
+    }
+
+}
