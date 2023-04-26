f19e27fbe0233fa4384b00d093f2ee6ffcfdbeea
Luxrie <saulmartinezsilva@outlook.com>
2023-04-26 13:39:10 -0300

    Sexo pessoa com expressoes lambda

---
+++ Mod22/src/Pessoas/Homem.java
@@ -0,0 +1,8 @@
+package Pessoas;
+
+public class Homem extends Pessoas{
+
+    public Homem(String nome, String sexo, int idade){
+        super(nome, sexo, idade);
+    }
+}
---
+++ Mod22/src/Pessoas/Pessoas.java
@@ -0,0 +1,38 @@
+package Pessoas;
+
+public abstract class Pessoas {
+
+    private String nome;
+    private String sexo;
+    private int idade;
+
+    public Pessoas(String nome, String sexo, int idade){
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
