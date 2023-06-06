0f2c2b7aa8178b21161ce6fd2f0fb592feff4d35
Saul M.Da Silva <94647646+Luxrie@users.noreply.github.com>
2023-06-06 14:21:17 -0300

    Springboot
    Springboot

---
+++ demo/src/main/java/com/example/demo/app.java
@@ -0,0 +1,13 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExemploController {
+
+    @GetMapping("/exemplo")
+    public String exemplo() {
+        return "Olá, Mundo!";
+    }
+}
